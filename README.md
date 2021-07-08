95b5190fba241a8787d6c7be390dabd898e2568e
cryptic-code <50461557+cryptic-code@users.noreply.github.com>
2021-07-08 20:54:27 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## A Dark Theme for Gedit based on Ayu-Dark Color Scheme
+
+### Installation
+
+To install for current user, place the `ayu-dark.xml` file inside `~/.local/share/gedit/styles` directory. Make the directory if it doesn't exist.
+
+Note: `~` is an alias for current user's home directory.
+
+Alternatively, you may also use the `install.sh` script.
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+home_dir=$(eval echo ~$(whoami))
+
+themes_dir="$home_dir/.local/share/gedit/styles/"
+
+if [ -d "$themes_dir" ]
+then
+    echo "Found local Gedit themes directory..."
+    cp ./ayu-dark.xml themes_dir
+    echo "Copied the theme file!"
+else
+    echo "Did not find local Gedit themes directory..."
+    echo "Making one..."
+    mkdir -p $themes_dir
+    cp ./ayu-dark.xml themes_dir
+    echo "Copied the theme file!"
+fi
